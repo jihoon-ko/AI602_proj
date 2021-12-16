079c560f71e34a8504c390258ca43bab8c29afee
Jihoon Ko <jihoonko@kaist.ac.kr>
2021-12-16 23:25:24 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -6,37 +6,46 @@ import torch_scatter
 from itertools import chain
 
 class GCN(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, D, K, k_1):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, D, L, K, model_type=0):
         super().__init__()
+        self.encoder = nn.Linear(input_dim, hidden_dim)
         self.num_layers = num_layers
         self.convs = torch.nn.ModuleList([GCNConv(hidden_dim if i > 0 else input_dim, hidden_dim if i < num_layers - 1 else output_dim) for i in range(num_layers)])
-        #self.quants = torch.nn.ModuleList([Quantization(hidden_dim, D, K, k_1) for i in range(num_layers)])
-        self.quantization = Quantization(hidden_dim, D, K, k_1)
+        self.quants = torch.nn.ModuleList([Quantization(hidden_dim, D, L, K) for i in range(num_layers)])
+        self.model_type = model_type
         
     def forward(self, data, tau=1.0):
         x, edge_index = data.x, data.edge_index
+        x_0, reg = self.encoder(x), 0
         for i in range(self.num_layers - 1):
             x = self.convs[i](x, edge_index)
-            x = F.leaky_relu(x, 0.5)
-            x = F.dropout(x, p=0.5, training=self.training)
-
-        _x, reg = self.quantization(x, tau)
-        x = x + _x
+            x = F.leaky_relu(x, 0.2)
+            x = F.dropout(x, p=0.3, training=self.training)
+            if self.model_type == 1:
+                _x, _reg = self.quants[i](x, tau)
+                x = x + _x
+                reg += _reg
+            elif self.model_type == 2:
+                x = x + x_0
+            elif self.model_type == 3:
+                _x, _reg = self.quants[i](x, tau)
+                x = x + _x + x_0
+                reg += _reg
 
         x = self.convs[-1](x, edge_index)
         return x, reg
     
 class Quantization(torch.nn.Module):
-    def __init__(self, hidden_dim, D, K, k_1):
+    def __init__(self, hidden_dim, D, L, K):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.D = D
+        self.L = L
         self.K = K
-        self.k_1 = k_1
-        self.register_buffer('k_inds', torch.LongTensor(list(chain.from_iterable([[j for _ in range(k_1 << j)] for j in range(K)]))))
-        self.register_buffer('k_blns', torch.FloatTensor(list(chain.from_iterable([[1.0 / (k_1 << j) for _ in range(k_1 << j)] for j in range(K)]))))
-        self.centroids = nn.Parameter(torch.FloatTensor(k_1 * ((1 << K) - 1), hidden_dim))
-        self.fc = nn.Linear(K, 1, bias=True)
+        self.register_buffer('k_inds', torch.LongTensor(list(chain.from_iterable([[j for _ in range(K << j)] for j in range(L)]))))
+        self.register_buffer('k_blns', torch.FloatTensor(list(chain.from_iterable([[1.0 / (K << j) for _ in range(K << j)] for j in range(L)]))))
+        self.centroids = nn.Parameter(torch.FloatTensor(K * ((1 << L) - 1), hidden_dim))
+        self.fc = nn.Linear(L, 1, bias=True)
         
         torch.nn.init.kaiming_normal_(self.centroids)
         
@@ -51,16 +60,15 @@ class Quantization(torch.nn.Module):
         soft_probs = torch_scatter.scatter_softmax(products / tau, self.k_inds, dim=1) # (#nodes, #ks, #D) -> (#nodes, #ks, #D)
         
         normalized_prods = soft_probs.unsqueeze(-1) * self.centroids.view(1, num_ks, self.D, -1) # (#nodes, #ks, #D, #hiddendim/D)
-        weighted_sums = torch_scatter.scatter(normalized_prods, self.k_inds, dim=1, dim_size=self.K, reduce='sum')
-        soft_feats = weighted_sums.view(num_nodes, self.K, -1)
+        weighted_sums = torch_scatter.scatter(normalized_prods, self.k_inds, dim=1, dim_size=self.L, reduce='sum')
+        soft_feats = weighted_sums.view(num_nodes, self.L, -1)
         
         quantized_feats = (hard_feats - soft_feats).detach() + soft_feats
-        #quantized_feats = soft_feats
         
         balance = torch.sum(soft_probs, 0) / num_nodes
         target_balance = self.k_blns.repeat(self.D).view(self.D, -1).T
-        reg = torch.norm(balance - target_balance)
-       
+        reg = torch.norm(balance - target_balance, p='fro')
+        
         pred = torch.max(quantized_feats, dim=1)[0]
         
         return pred, reg
--- node_cls.py
+++ node_cls.py
@@ -9,36 +9,39 @@ def main(args):
     dataset = Planetoid(root=f'/tmp/{args.dataset}', name=args.dataset)
     data = dataset[0].to(device)
     
-    model = GCN(dataset.num_node_features, args.hidden_dim, dataset.num_classes, args.num_layers, D=args.D, K=args.K, k_1=args.K1).to(device)
+    model = GCN(dataset.num_node_features, args.hidden_dim, dataset.num_classes, args.num_layers, D=args.D, L=args.L, K=args.K, model_type=args.model_type).to(device)
     print(model)
     
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     
-    model.train()
     max_acc = -1
     max_acc_val = -1
     for epoch in range(args.num_epochs):
+        model.train()
         optimizer.zero_grad()
-        out, reg1 = model(data, args.tau)
-        loss = loss_fn(out[data.train_mask], data.y[data.train_mask]) + args.lmbda * reg1
+        out, reg = model(data, args.tau)
+        loss = loss_fn(out[data.train_mask], data.y[data.train_mask]) + args.lmbda * reg
         loss.backward()
         optimizer.step()
         
         model.eval()
-        pred = model(data)[0].argmax(dim=1)
+        pred = model(data, args.tau)[0].argmax(dim=1)
         correct_val = (pred[data.val_mask] == data.y[data.val_mask]).sum()
         correct_test = (pred[data.test_mask] == data.y[data.test_mask]).sum()
         acc_val = int(correct_val) / int(data.val_mask.sum())
         acc_test = int(correct_test) / int(data.test_mask.sum())
+        
         if acc_val >= max_acc_val:
-            torch.save(model.state_dict(), 'models/dim_{}_nl_{}_K_{}_K1_{}_D_{}_tau_{}_lmbda_{}.pkt'.format(args.hidden_dim, args.num_layers, args.K, args.K1, args.D, args.tau, args.lmbda))
+        #    torch.save(model.state_dict(), 'models/dim_{}_nl_{}_K_{}_K1_{}_D_{}_tau_{}_lmbda_{}.pkt'.format(args.hidden_dim, args.num_layers, args.K, args.K1, args.D, args.tau, args.lmbda))
             max_acc_val, max_acc = acc_val, acc_test
+        
         print('Valid Accuracy: {:.3f}\t(Best Test Accuracy: {:.3f})'.format(acc_val, max_acc))
     
-    with open('log.txt', 'a') as f:
-        f.write('dim_{}_nl_{}_K_{}_K1_{}_D_{}_tau_{}_lmbda_{}.txt'.format(args.hidden_dim, args.num_layers, args.K, args.K1, args.D, args.tau, args.lmbda) + '\n')
+    with open('log_data_{}_type_{}.txt'.format(args.dataset, args.model_type), 'a') as f:
+        f.write('dim_{}_nl_{}_L_{}_K_{}_D_{}_tau_{}_lmbda_{}'.format(args.hidden_dim, args.num_layers, args.L, args.K, args.D, args.tau, args.lmbda) + '\n')
         f.write(str(max_acc) + '\n\n')
+    
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
@@ -46,17 +49,22 @@ if __name__ == '__main__':
     parser.add_argument("--dataset", type=str, default='cora', help="dataset name (cora/citeseer/pubmed)")
     parser.add_argument("--model", type=str, default='gcn', help="model name")
     parser.add_argument("--hidden-dim", type=int, default=16, help="hidden dimension for model")
-    parser.add_argument("--num-layers", type=int, default=2, help="number of the layers")
+    parser.add_argument("--num-layers", type=int, default=5, help="number of the layers")
     parser.add_argument("--lr", type=float, default=0.01, help="learning rate")
     parser.add_argument("--weight-decay", type=float, default=5e-4, help="regularization (weight decay)")
-    parser.add_argument("--num-epochs", type=int, default=300, help="the number of training epochs")
+    parser.add_argument("--num-epochs", type=int, default=200, help="the number of training epochs")
     parser.add_argument("--gpu", type=int, default=7, help="gpu id")
     
     # Hyperparameters for the proposed model
-    parser.add_argument("--K", type=int, default=5, help="number of clusters")
-    parser.add_argument("--K1", type=int, default=4, help="size of cluster 1")
+    parser.add_argument("--L", type=int, default=2, help="number of levels")
+    parser.add_argument("--K", type=int, default=32, help="size of cluster 1")
     parser.add_argument("--D", type=int, default=4, help="number of division?")
     parser.add_argument("--tau", type=float, default=1.0, help="softmax temperature")
     parser.add_argument("--lmbda", type=float, default=1.0, help="regularization parameter")
+    parser.add_argument("--model_type", type=int, default=0, help="model type")
     args=parser.parse_args()
+    
+    if args.lmbda > 0.01:
+        exit(0)
+    
     main(args)
